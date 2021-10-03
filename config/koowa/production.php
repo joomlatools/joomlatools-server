0fd35f2a441bdee817063a64934e06d6ed7f4281
Johan Janssens <johan@janssens.me>
2021-10-03 17:38:11 +0200

    #1 - Initial import

---
+++ config/koowa/production.php
@@ -0,0 +1,17 @@
+<?php
+/* Production */
+ini_set('display_errors', 0);
+
+require_once dirname(__DIR__).'/sentry.php';
+
+return [
+    'sitename'   => 'Joomlatools - Pages Server',
+    'caching'    => 1,
+
+    'mailer'     => 'smtp',
+    'smtphost'   => '',
+    'smtpport'   => '',
+    'smtpauth'   => '',
+    'smtpuser'   => '',
+    'smtppass'   => ''
+];
---
+++ config/pages.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+    'http_cache' => getenv('APP_CACHE') !== false ? filter_var(getenv('APP_CACHE'), FILTER_VALIDATE_BOOLEAN) : true,
+    'http_cache_time'         => '1week',
+    'http_cache_time_browser' => '1day',
+
+    'http_static_cache'    => false,
+    'http_client_cache'    => getenv('APP_CACHE') !== false ? filter_var(getenv('APP_CACHE'), FILTER_VALIDATE_BOOLEAN) : true,
+
+    //'page_cache_validation'     => filter_var(getenv('APP_CACHE'), FILTER_VALIDATE_BOOLEAN),
+    //'template_cache_validation' => filter_var(getenv('APP_CACHE'), FILTER_VALIDATE_BOOLEAN),
+    //'data_cache_validation'     => filter_var(getenv('APP_CACHE'), FILTER_VALIDATE_BOOLEAN),
+
+    'sites' => [
+        '[*]' => KOOWA_ROOT.'/sites/default',
+    ],
+
+    //See: https://www.owasp.org/index.php/OWASP_Secure_Headers_Project#xpcdp
+    'headers' => [
+        'Strict-Transport-Security' => getenv('APP_ENV') == 'production' ? 'max-age=63072000;' : null,
+        'X-Xss-Protection'          => '1; mode=block',
+        'X-Frame-Options'           => 'DENY',
+        'Feature-Policy'            => "camera 'none'; microphone 'none'",
+        'Referrer-Policy'           => 'strict-origin-when-cross-origin',
+        'X-Permitted-Cross-Domain-Policies' => 'none',
+        'Content-Security-Policy'   => in_array(getenv('APP_ENV'), ['production', 'staging']) ? 'upgrade-insecure-requests' : null,
+        'X-Content-Type-Options'    => 'nosniff',
+    ],
+
+    'aliases' => [
+        'media://koowa/framework/css/debugger.css' => 'https://files.joomlatools.com/joomlatools-framework@3.5.0/resources/assets/css/debugger.min.css',
+        'media://koowa/framework/js/debugger.js' => 'https://files.joomlatools.com/joomlatools-framework@3.5.0/resources/assets/js/debugger.min.js',
+    ],
+);
