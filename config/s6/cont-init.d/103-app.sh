9a01fb3b3b496c4b55a17f9c21f59aab95ee7473
Johan Janssens <johan@janssens.me>
2021-10-12 14:00:28 +0200

    #20 - Add MYSQL_ENABLE env variable

---
+++ config/s6/cont-init.d/103-app.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bash
+
+echo "MySQL Configuration"
+echo "> MYSQL_ENABLE = "${MYSQL_ENABLE:-undefined}
+echo "> MYSQL_USER   = "${MYSQL_USER:-undefined}
+echo "> MYSQL_VOLUME = "${MYSQL_VOLUME:-undefined}
--- config/s6/cont-init.d/110-mysql.sh
+++ config/s6/cont-init.d/110-mysql.sh
@@ -5,6 +5,12 @@
 
 file="${0##*/}"
 
+if [[ $MYSQL_ENABLE -eq 1 ]] ; then
+  mkdir -p /var/run/s6/services/mysql
+  printf "1" > /var/run/s6/services/mysql/down
+  exit
+fi
+
 MYSQL_PID_FILE=/var/run/mysqld/mysqld.pid
 MYSQL_RUN_DIR=/var/run/mysqld
 
@@ -28,6 +34,8 @@ chmod 1777 $MYSQL_RUN_DIR
 if [[ ! -d $MYSQL_VOLUME ]]; then
 
   echo "[cont-init.d] ${file}: Installing MySQL in ${MYSQL_VOLUME} ..."
+
+  mkdir -p $MYSQL_VOLUME
   /usr/bin/mysqld_safe --initialize-insecure --datadir=${MYSQL_VOLUME}
 
 fi
