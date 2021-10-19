26afd5d271e753dbb480aebe774fe4a6856a66b2
Johan Janssens <johan@janssens.me>
2021-10-20 01:41:49 +0200

    #30 - Cleanup

--- sites/foo/config.php
+++ sites/foo/config.php
@@ -4,7 +4,6 @@ return [
 
     'cache_path' => getenv('APP_VOLUME').'/foo/cache',
     'log_path'   => getenv('APP_VOLUME').'/foo/log',
-    'url_prefix' => '', //remove index.php
 
     'aliases' => [
         'theme://'  => 'base://theme/',
