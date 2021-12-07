76cfec9d34cb30d9019a14ee2bdfb3ae0b1623b4
Johan Janssens <johan@janssens.me>
2021-12-07 13:59:55 +0100

    #49 - Enable Sentry

--- config/koowa/development.php
+++ config/koowa/development.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
+// Set environment config
 return [
     'sitename'   => 'Joomlatools Server [development]',
     'caching'    => 0,
