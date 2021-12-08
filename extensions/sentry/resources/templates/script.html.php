f15e6abf2eef9ff3e89f8981aed8ce7655e9f6b9
Johan Janssens <johan@janssens.me>
2021-12-08 16:17:34 +0100

    #49 - Add tunnel config option, NULL by default

--- extensions/sentry/resources/templates/script.html.php
+++ extensions/sentry/resources/templates/script.html.php
@@ -10,6 +10,7 @@
 
 dsn:
 version:
+tunnel:
 tags: []
 tracesSampleRate: 1.0
 ---
@@ -20,32 +21,33 @@ $dsn = $dsn ?? getenv('SENTRY_DSN');
 if(!empty($version)) {
     $version =  '@'.$version;
 }
+
 ?>
 
 <? if (!empty($dsn)): ?>
-    <script src="https://unpkg.com/@sentry/tracing<?= $version ?>/build/bundle.tracing.min.js" crossorigin="anonymous" ></script>
-    <script>
-        Sentry.init({
-            dsn: "<?= $dsn ?>",
-            debug: <?= debug() ?>,
-            environment: "<?= getenv('SENTRY_ENVIRONMENT') ?: getenv('APP_ENV') ?>",
-            tracesSampleRate: <?= $tracesSampleRate ?? 1.0 ?>,
-            integrations: [new Sentry.Integrations.BrowserTracing()],
-            initialScope: scope => {
-                <?
+<script src="https://unpkg.com/@sentry/tracing<?= $version ?>/build/bundle.tracing.min.js" crossorigin="anonymous" ></script>
+<script>
+Sentry.init({
+    dsn: "<?= $dsn ?>",
+    debug: <?= debug() ?>,
+    tunnel: <?= !empty($tunnel) ? '"'.$tunnel.'"' : null ?>,
+    environment: "<?= getenv('SENTRY_ENVIRONMENT') ?: getenv('APP_ENV') ?>",
+    tracesSampleRate: <?= $tracesSampleRate ?? 1.0 ?>,
+    integrations: [new Sentry.Integrations.BrowserTracing()],
+    initialScope: scope => {
+        <?
+        if(getenv('FLY_REGION')) {
+            $tags['app.region'] = getenv('FLY_REGION');
+        }
 
-                if(getenv('FLY_REGION')) {
-                    $tags['app.region'] = getenv('FLY_REGION');
-                }
+        if(getenv('FLY_ALLOC_ID')) {
+            $tags['app.id'] =  hash('crc32b', getenv('FLY_ALLOC_ID'));
+        }
+        ?>
 
-                if(getenv('FLY_ALLOC_ID')) {
-                    $tags['app.id'] =  hash('crc32b', getenv('FLY_ALLOC_ID'));
-                }
-                ?>
-
-                scope.setTags(<?= json($tags) ?>);
-                return scope;
-            },
-        });
-    </script>
+        scope.setTags(<?= json($tags) ?>);
+        return scope;
+    },
+});
+</script>
 <? endif; ?>
