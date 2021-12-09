a625a026742d8d7e75190aae43186bb8adcdd707
Johan Janssens <johan@janssens.me>
2021-12-09 13:19:24 +0100

    #49 - Add release config option, fall back to SENTRY_RELEASE or Pages Version if not defined

--- extensions/sentry/resources/templates/script.html.php
+++ extensions/sentry/resources/templates/script.html.php
@@ -10,6 +10,7 @@
 
 dsn:
 version:
+release:
 tunnel:
 tags: []
 environment:
@@ -19,6 +20,7 @@ tracesSampleRate: 1.0
 <?
 $dsn = $dsn ?? getenv('SENTRY_DSN');
 $env = $environment ?? getenv('SENTRY_ENVIRONMENT');
+$rel = $release ?? getenv('SENTRY_RELEASE') ?: object('pages.version')->getVersion();
 
 if(!empty($version)) {
     $version =  '@'.$version;
@@ -33,6 +35,7 @@ Sentry.init({
     dsn: "<?= $dsn ?>",
     debug: <?= debug() ? 'true' : 'false' ?>,
     tunnel: <?= !empty($tunnel) ? '"'.$tunnel.'"' : 'null' ?>,
+    release: <?= !empty($rel) ? '"'.$rel.'"' : 'null' ?>,
     environment: <?= !empty($env) ? '"'.$env.'"' : 'null' ?>,
     tracesSampleRate: <?= $tracesSampleRate ?? 1.0 ?>,
     integrations: [new Sentry.Integrations.BrowserTracing()],
