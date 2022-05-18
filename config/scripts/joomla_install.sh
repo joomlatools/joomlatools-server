4289c9769bd6b3e0acf176e54054494720f36281
Johan Janssens <johan@janssens.me>
2022-05-18 23:21:01 +0200

    #52 - Add Joomla install script

---
+++ config/scripts/joomla_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [ -d "$APP_ROOT/sites/joomla" ]; then
+    exit 0
+fi;
+
+joomla plugin:install joomlatools/console-joomlatools:dev-master
+
+echo "* Downloading Joomla"
+
+joomla site:download ${JOOMLA_NAME} --www=$APP_ROOT/sites --release=${JOOMLA_VERSION}
+
+#wait for the database to ready
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
+
+echo "* Creating the Joomla database"
+joomla database:install ${JOOMLA_NAME} --www=$APP_ROOT/sites --drop --mysql-login=root:
+
+echo "* Configuring Joomla"
+joomla site:configure ${JOOMLA_NAME} --www=$APP_ROOT/sites --overwrite --mysql-login=root:
