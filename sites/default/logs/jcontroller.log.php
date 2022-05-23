08dcddcbb34673a812f35df7d6c2a105cc5e31a7
Johan Janssens <johan@janssens.me>
2022-05-23 11:57:07 +0200

    Cleanup

--- sites/default/logs/jcontroller.log.php
+++
@@ -1,9 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2022-05-22 22:12:52 UTC
-#Software: Joomla! 4.1.3 Stable [ Kuamini ] 10-May-2022 16:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2022-05-22T22:12:52+00:00	INFO 172.22.0.1	controller	Holding edit ID com_categories.edit.category.2 Array (     [0] => 2 ) 
-2022-05-22T22:12:52+00:00	INFO 172.22.0.1	controller	Checking edit ID com_categories.edit.category.2: 1 Array (     [0] => 2 ) 
-2022-05-22T22:13:00+00:00	INFO 172.22.0.1	controller	Releasing edit ID com_categories.edit.category.2 Array ( ) 
