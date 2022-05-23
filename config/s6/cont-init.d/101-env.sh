4d8aa46bda84a31bb53dbda8540f690ac1408186
Johan Janssens <johan@janssens.me>
2022-05-23 13:02:27 +0200

    #53 - Cleanup environment variables, set default for development purposes

--- config/s6/cont-init.d/101-env.sh
+++ config/s6/cont-init.d/101-env.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/with-contenv bash
 
-# Set environment defaults
-MYSQL_ENABLE=${MYSQL_ENABLE:=0}
+APP_NAME=${APP_NAME:=joomlatools-server}
+printf "%s" $APP_NAME > /var/run/s6/container_environment/APP_NAME
+
+APP_ENV=${APP_ENV:=development}
+printf "%s" $APP_ENV > /var/run/s6/container_environment/APP_ENV
+
+APP_DEBUG=${APP_DEBUG:=on}
+printf "%s" $APP_DEBUG > /var/run/s6/container_environment/APP_DEBUG
+
+APP_CACHE=${APP_CACHE:=off}
+printf "%s" $APP_CACHE > /var/run/s6/container_environment/APP_CACHE
+
+APP_PRELOAD=${APP_PRELOAD:=off}
+printf "%s" $APP_PRELOAD > /var/run/s6/container_environment/APP_PRELOAD
+
+MYSQL_ENABLE=${MYSQL_ENABLE:=1}
 printf "%s" $MYSQL_ENABLE > /var/run/s6/container_environment/MYSQL_ENABLE
 
 MYSQL_USER=${MYSQL_USER:=root}
@@ -10,7 +24,7 @@ printf "%s" $MYSQL_USER > /var/run/s6/container_environment/MYSQL_USER
 MYSQL_VOLUME=${MYSQL_VOLUME:=/mnt/www/mysql}
 printf "%s" $MYSQL_VOLUME > /var/run/s6/container_environment/MYSQL_VOLUME
 
-JOOMLA_SITE=${JOOMLA_SITE:=joomla}
+JOOMLA_SITE=${JOOMLA_SITE:=default}
 printf "%s" $JOOMLA_SITE > /var/run/s6/container_environment/JOOMLA_SITE
 
 JOOMLA_VERSION=${JOOMLA_VERSION:=latest}
