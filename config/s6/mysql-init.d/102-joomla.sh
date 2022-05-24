502e04967ff49f82f45a6fa99e62e47c1c3511fa
Ercan Ozkaya <ercan@timble.net>
2022-05-24 17:26:21 +0300

    #59: Move Joomla script inline

--- config/s6/mysql-init.d/102-joomla.sh
+++ config/s6/mysql-init.d/102-joomla.sh
@@ -4,8 +4,18 @@ DIR=${APP_ROOT:=/var/www}/sites/${JOOMLA_SITE}
 if [[ ! -d $DIR ]];
 then
   echo "Installing Joomla..."
-  chmod u+x /var/scripts/joomla_install.sh
-  /var/scripts/joomla_install.sh
+
+  echo "* Downloading Joomla"
+  joomla site:download ${JOOMLA_SITE} --release=${JOOMLA_VERSION}
+
+  echo "* Creating the Joomla database"
+  joomla database:install ${JOOMLA_SITE} --drop --mysql-login=root:
+
+  echo "* Configuring Joomla"
+  joomla site:configure ${JOOMLA_SITE} --overwrite --mysql-login=root:
+
+  echo "* Creating vhost"
+  joomla vhost:create ${JOOMLA_SITE} 
 else
   echo "Installing Joomla... skipped"
 fi
