3f3c3397ffe61f937b7b319e6f9ddfd9085f6049
Ercan Ozkaya <ercan@timble.net>
2022-05-25 19:52:47 +0300

    #64: Assign the MYSQL_pass environment variable to both root@% and root@localhost users

--- config/s6/mysql-init.d/101-perm.sh
+++ config/s6/mysql-init.d/101-perm.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/with-contenv bash
 
 echo "[myql-init.d] ${file}: Granting local access of MySQL database from localhost for ${MYSQL_USER}"
-/usr/bin/mysql -e "
-  CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%';
- "
 
 if [[ $APP_ENV = "development" ]]
 then
   echo "[mysql-init.d] ${file}: Granting remote access of MySQL database from any IP address for ${MYSQL_USER}"
   /usr/bin/mysql -e "
     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%';
+    ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASS}';
+    ALTER USER '${MYSQL_USER}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASS}';
     GRANT ALL ON *.* TO '${MYSQL_USER}'@'%';
     FLUSH PRIVILEGES;
     "
