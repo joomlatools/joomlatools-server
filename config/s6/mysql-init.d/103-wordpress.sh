b9456b8bdb025dc91742bb2bd7aeadc5d9368580
Johan Janssens <johan@janssens.me>
2022-06-01 16:58:59 +0200

    Chown commands no longer required

--- config/s6/mysql-init.d/103-wordpress.sh
+++ config/s6/mysql-init.d/103-wordpress.sh
@@ -6,7 +6,6 @@ then
   echo "Installing Wordpress..."
 
   folioshell site:create ${WORDPRESS_SITE} --release=${WORDPRESS_VERSION}
-  chown -R ${APP_USER}:${APP_USER} $DIR
 
 else
   echo "Installing Wordpress... skipped"
