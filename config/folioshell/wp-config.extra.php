e5a27bfcbb965046e1c2f21b8192668f369bcbc7
Ercan Ozkaya <ercan@timble.net>
2022-06-02 11:20:35 +0300

    Guard against missing server key

--- config/folioshell/wp-config.extra.php
+++ config/folioshell/wp-config.extra.php
@@ -7,7 +7,7 @@ if (getenv('APP_NAME') === 'joomlatools-server' && isset($_SERVER['HTTP_HOST']))
     $scheme = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? ($_SERVER['REQUEST_SCHEME'] ?? 'http');
     $host   = $_SERVER['HTTP_HOST'];
 
-    $url = $scheme.'://'.$host . rtrim($_SERVER['HTTP_X_SITE_BASE'], '/\\');
+    $url = $scheme.'://'.$host . (isset($_SERVER['HTTP_X_SITE_BASE']) ? rtrim($_SERVER['HTTP_X_SITE_BASE'], '/\\') : '');
 
     define('WP_SITEURL', $url);
     define('WP_HOME', $url);
