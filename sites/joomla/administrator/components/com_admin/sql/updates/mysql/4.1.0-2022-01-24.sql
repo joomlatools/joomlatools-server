d08d3a5b3e7fbc91983c2f61ace696d91ef6a1bd
Johan Janssens <johan@janssens.me>
2022-06-05 01:27:29 +0200

    #55 - Remove wrongly committed site

--- sites/joomla/administrator/components/com_admin/sql/updates/mysql/4.1.0-2022-01-24.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `#__redirect_links` DROP INDEX `idx_link_modifed`;
-ALTER TABLE `#__redirect_links` ADD INDEX `idx_link_modified` (`modified_date`);
--- sites/joomla/administrator/components/com_admin/sql/updates/postgresql/4.1.0-2022-01-24.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS "#__redirect_links_idx_link_modifed";
-CREATE INDEX "#__redirect_links_idx_link_modified" ON "#__redirect_links" ("modified_date");
