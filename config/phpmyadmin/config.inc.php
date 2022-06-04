a5728a455e98ce5cd2078e135fb3d2bdb7d69886
Johan Janssens <johan@janssens.me>
2022-06-05 01:09:42 +0200

    #21 - Add config for phpmyadmin

---
+++ config/phpmyadmin/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * phpMyAdmin sample configuration, you can use it as base for
+ * manual configuration. For easier setup you can use setup/
+ *
+ * All directives are explained in documentation in the doc/ folder
+ * or at <https://docs.phpmyadmin.net/>.
+ */
+
+declare(strict_types=1);
+
+$cfg['AllowThirdPartyFraming'] = true;
