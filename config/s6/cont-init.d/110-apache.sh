5ba72122586dd16faf43ee91a4b2ffea3fd2c1fa
Johan Janssens <johan@janssens.me>
2022-06-07 23:56:09 +0200

    Split vhosts into different files

--- config/s6/cont-init.d/110-apache.sh
+++ config/s6/cont-init.d/110-apache.sh
@@ -2,3 +2,7 @@
 
 # Setup modules
 a2enmod vhost_alias -q | sed "s/^/[cont-init.d] ${file}: /"
+
+# Setup default sites
+a2ensite 100-cloudflare -q | sed "s/^/[cont-init.d] ${file}: /"
+a2ensite 101-gitpod -q | sed "s/^/[cont-init.d] ${file}: /"
