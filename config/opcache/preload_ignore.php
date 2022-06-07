74bc4c6a0dfe0423531688b55920d45e7b6390a8
Johan Janssens <johan@janssens.me>
2022-06-07 15:25:49 +0200

    Exclude framework and pages from preloading

--- config/opcache/preload_ignore.php
+++ config/opcache/preload_ignore.php
@@ -5,4 +5,7 @@ return [
     '/symfony/console',
     '/phpmailer/phpmailer/language',
     '/joomlatools/framework/code/libraries/joomlatools/vendor/composer/ClassLoader.php',
+    #-----------------------------------------------------
+    '/joomlatools/framework', //Not compatible with PHP8.1
+    '/joomlatools/pages',     //Not compatible with PHP8.1
 ];
