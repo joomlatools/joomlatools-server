d1e8741caae8ec144ca5121f62e3646aefdf4121
Johan Janssens <johan@janssens.me>
2022-06-09 04:12:03 +0200

    Refactor Apache vhosts and Wordpress site routing

--- config/folioshell/wp-config.extra.php
+++ config/folioshell/wp-config.extra.php
@@ -1,5 +1,8 @@
-if (getenv('APP_NAME') === 'joomlatools-server' && isset($_SERVER['HTTP_HOST']))
+if (isset($_SERVER['SITE_ROOT']))
 {
+    $site    = trim(str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']), '/');
+    $segments = explode('/', $site);
+
     if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
         $_SERVER['HTTPS'] = 'on';
     }
@@ -7,14 +10,26 @@ if (getenv('APP_NAME') === 'joomlatools-server' && isset($_SERVER['HTTP_HOST']))
     $scheme = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? ($_SERVER['REQUEST_SCHEME'] ?? 'http');
     $host   = $_SERVER['HTTP_HOST'];
 
-    $url = $scheme.'://'.$host . (isset($_SERVER['HTTP_X_SITE_BASE']) ? rtrim($_SERVER['HTTP_X_SITE_BASE'], '/\\') : '');
+    if($segments && is_dir($_SERVER['SITE_ROOT'].'/'.$segments[0])) {
+        $url = $scheme.'://'.$host . '/'. $segments[0];
+    } else {
+        $url = $scheme.'://'.$host;
+    }
 
     define('WP_SITEURL', $url);
     define('WP_HOME', $url);
 
     require_once ABSPATH . 'wp-includes/plugin.php';
 
+    add_filter('do_redirect_guess_404_permalink', function($result) {
+        return false;
+    }, 100000, 1);
+
     add_filter('option_siteurl', function($option) {
         return WP_SITEURL;
+    });
+
+    add_filter('option_home', function($option) {
+        return WP_HOME;
     });
 }
