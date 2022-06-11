dc0dfa1c0e30c69e94cf3b57e6664628f5c7ba92
Johan Janssens <johan@janssens.me>
2022-06-12 00:51:45 +0200

    Sync with changes in jt-base

--- config/s6/cont-init.d/110-apache.sh
+++ config/s6/cont-init.d/110-apache.sh
@@ -4,5 +4,5 @@
 a2enmod vhost_alias -q | sed "s/^/[cont-init.d] ${file}: /"
 
 # Setup default sites
-a2ensite 100-cloudflare -q | sed "s/^/[cont-init.d] ${file}: /"
 a2ensite 101-gitpod -q | sed "s/^/[cont-init.d] ${file}: /"
+a2ensite 102-cloudflare -q | sed "s/^/[cont-init.d] ${file}: /"
